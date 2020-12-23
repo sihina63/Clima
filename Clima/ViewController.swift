6d54ae87ca5178f905fa2c1f94588a0a3b0a62ee
sihina63 <67451905+sihina63@users.noreply.github.com>
2020-12-23 13:18:46 +0900

    Initial Commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by mahesh mannapperuma on R 2/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
